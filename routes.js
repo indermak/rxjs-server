12b346bac8fb5d88d3a41abf82d960b8c9654b8b
indermak <i.mak1992@gmail.com>
2018-07-27 14:09:09 +0530

    Removed unneccessary code

--- routes.js
+++ routes.js
@@ -15,8 +15,6 @@ const users = require('./app/users');
 // router.use('/email/webhook', verifyWebhookToken);
 // End Webhook Routes
 
-router.post("/", users.addUser);
-router.get("/messages", users.getMessages);
 router.get("/user", users.getDetail);
 router.post("/user", users.searchUser);
 
